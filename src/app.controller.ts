434176b3679ca65ceec730cc6fc4f3c709688c66
sameekruth27 <sameekruth@gmail.com>
2024-07-14 04:41:53 +0530

    added welcome msg

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,9 +4,4 @@ import { AppService } from './app.services';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return `Hello from Todo REST API!`;
-  }
 }
