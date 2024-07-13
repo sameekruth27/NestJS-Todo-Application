8a87a014b0ad90350a3e19bccae6376d3e460b10
sameekruth27 <sameekruth@gmail.com>
2024-07-14 04:28:01 +0530

    add endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { TodoModule } from './todo/todo.module';
 import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.services';
 
 @Module({
   imports: [DatabaseModule, TodoModule, AuthModule],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
