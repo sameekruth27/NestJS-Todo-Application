e7fdd651295c5b08d453e0cc110b1f8e3219c163
sameekruth27 <sameekruth@gmail.com>
2024-07-14 04:47:58 +0530

    added welcome msg

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,4 +4,14 @@ import { AppService } from './app.services';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/swagger')
+  getSwaggerPage(): string {
+    return this.appService.getSwaggerPage();
+  }
 }
--- src/app.services.ts
+++ src/app.services.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hey There! Thanks for visiting my Todo rest API site! You can access the Todo functionality here: <a href="https://nestjs-todo-sk.vercel.app/swagger">https://nestjs-todo-sk.vercel.app/swagger</a>';
   }
+
+  getSwaggerPage(): string {
+    return 'Welcome to the Todo API!';
+  }
 }
