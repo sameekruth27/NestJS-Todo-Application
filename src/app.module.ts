06dfe748a96bdb55286f4a7c8a887643cf60550a
sameekruth27 <sameekruth@gmail.com>
2024-07-14 04:30:34 +0530

    add welcome msg

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { TodoModule } from './todo/todo.module';
 import { AuthModule } from './auth/auth.module';
-import { AppController } from './app.controller';
 import { AppService } from './app.services';
 
 @Module({
   imports: [DatabaseModule, TodoModule, AuthModule],
-  controllers: [AppController],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
